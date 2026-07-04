11cb94f17315d3af07001fcdae21c0a4714301a9
GDC2026 Programming <gdc2026-programming@unf.dk>
2026-07-04 14:13:17 +0200

    Del 1 og 2, samt begyndelsen af del 3

---
+++ Assets/Player.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    [SerializeField] int speed;
+
+    [SerializeField] int gunCooldown;
+
+    [SerializeField] InputAction moveAction;
+
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Im player");
+        rb = GetComponent<Rigidbody>();
+        moveAction.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveDir = moveAction.ReadValue<Vector2>();
+        
+        Vector3 
+        
+    }
+}
